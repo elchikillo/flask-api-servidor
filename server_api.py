0620291aa81c6b81f48b176216bb45c21cfb1343
elchikillo <erick2910diego04@gmail.com>
2025-07-25 14:50:49 -0500

    Usar puerto dinámico para Render

--- server_api.py
+++ server_api.py
@@ -60,4 +60,6 @@ def obtener_ultima_imagen():
         return f"Error en la base de datos: {err}", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
